0826e4462d1a6687f972c4642489d191c49b33ef
keisukeisobe <52838946+keisukeisobe@users.noreply.github.com>
2020-03-05 16:03:42 -0500

    added sets table/migrations/seeds

--- seeds/seed.users.sql
+++ seeds/seed.users.sql
@@ -8,4 +8,8 @@ INSERT INTO users (username, password, email)
 VALUES
   ('dunder', '$2a$12$/CwJvVbQbC7Bt2FMkSo.Be3vzvtT80Jt6eDr4acBAdk3DOlOhMU4K', 'info1@email.com');
 
+INSERT INTO sets (user_id, add_date, reps, weight, exercise, e1rm)
+VALUES
+  (1, '2020-03-05', 10, 225, 'bench press', 300);
+
 COMMIT;
